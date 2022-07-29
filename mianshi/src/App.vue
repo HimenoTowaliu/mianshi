2a550f2cdca9a5f7eed9bc23db73b7ee54a4ab86
liuyang <1935845650@qq.com>
2022-07-29 22:59:07 +0800

    2022/7/29 22:59创建vue项目并且看到js里的运算符了

---
+++ mianshi/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Js></Js>
+  </div>
+</template>
+
+<script>
+import Js from './components/Js/js.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Js
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ mianshi/src/components/Js/js.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="showobject">点击遍历对象</button>
+    <img src="../../assets/当前进度.png" alt="">
+    看完数据该看运算符
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Js',
+  data() {
+    return {
+      obj1: {
+        name: 'liuyang',
+        age: 18,
+        role: 'student'
+      }
+    }
+  },
+  methods: {
+    showobject() {
+      for (let key in this.obj1) {
+        console.log('obj1的属性名：' + key)
+        console.log('obj1的属性值:' + this.obj1[key])
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
